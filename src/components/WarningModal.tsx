b0c88c4c3b72344c54f90cc7bd551e811abf8d9c
siuwafong <wilsonfong1002@outlook.com>
2026-05-11 17:28:28 -0400

    fix type error

--- src/components/WarningModal.tsx
+++ src/components/WarningModal.tsx
@@ -13,7 +13,7 @@ import {
 } from "@/components/ui/dialog"
 import { CircleX } from "lucide-react"
 
-export const WarningModal = ({ onConfirm, onCancel, title, description, onClick }: { onConfirm: () => void; onCancel: () => void, title: string, description: string, onClick: () => void }) => {
+export const WarningModal = ({ onConfirm, onCancel, title, description, onClick }: { onConfirm: () => void; onCancel?: () => void, title: string, description: string, onClick: () => void }) => {
     return (
         <Dialog>
                 <DialogTrigger asChild>
@@ -30,7 +30,7 @@ export const WarningModal = ({ onConfirm, onCancel, title, description, onClick 
                     </DialogHeader>
                     <DialogFooter>
                 <DialogClose asChild>
-                    <Button onClick={onCancel} variant="outline">Cancel</Button>
+                    <Button onClick={onCancel ? () => onCancel() : () => null} variant="outline">Cancel</Button>
                 </DialogClose>      
                 <DialogClose asChild>                 
                     <Button variant="destructive" onClick={onConfirm}>Save changes</Button>
